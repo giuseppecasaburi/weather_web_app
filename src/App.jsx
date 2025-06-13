18cfeb9d1bf11dce7a0194771dad4d8545aebe82
giuseppecasaburi <enzobiondi0000@libero.it>
2025-06-13 14:13:27 +0200

    feat: Aggiungi routing e stili per la header dell'applicazione

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from "./components/Layout"
+import Dashboard from "./pages/Dashboard"
+import SettingsPage from "./pages/SettingsPage"
+
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="/settings" element={<SettingsPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
     return (
         <>
         <header>
             <div className="container">
-                <h3>Header</h3>
+                <div id="nav-link">
+                    <NavLink className="nav-link-elem" to={"/"}>Dashboard</NavLink>
+                    <NavLink className="nav-link-elem" to={"/settings"}>Settings</NavLink>
+                </div>
+
             </div>
         </header>
         </>
